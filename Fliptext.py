486d5c769f3b1b1bd4c44742fc6f36926bc9a060
Liam Stanley <me@liamstanley.net>
2013-12-28 17:44:22 -0500

    Update Fliptext.py

--- Fliptext.py
+++ Fliptext.py
@@ -1,11 +1,11 @@
-__module_name__ = 'Backwards Text'
+__module_name__ = 'Flip Text'
 __module_version__ = '0.1'
-__module_description__ = 'Turns your text backwards.'
-__module_author__ = 'Jake0720, with a little help of Liam Stanley'
+__module_description__ = 'Inverts your text!'
+__module_author__ = 'Jake0720, with a little help from Liam Stanley'
 
 import xchat
 
-fliphelp = 'Type /btext <message> then press enter to make it backwards.'
+help = 'Type /flip <message> then press enter to make it backwards.'
 
 print('%s has been loaded.' % __module_name__)
 
@@ -13,10 +13,10 @@ def flip(word, word_eol, userdata):
     try:
         xchat.command('say %s' % word_eol[1][::-1])
     except:
-        xchat.prnt('Error')
+        xchat.prnt(help)
 
 def onUnload(userdata):
     xchat.prnt('%s has been unloaded.' % __module_name__)
 
-xchat.hook_command('btext', flip, help=fliphelp)
+xchat.hook_command('flip', flip, help=help)
 xchat.hook_unload(onUnload)

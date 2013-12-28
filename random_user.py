c1206c8daf76e515b02f36e1868fb5251246455b
Liam Stanley <me@liamstanley.net>
2013-12-28 18:14:19 -0500

    New "Random-User" Plugin.

---
+++ random_user.py
@@ -0,0 +1,26 @@
+__module_name__ = 'Random-User'
+__module_version__ = '0.1'
+__module_description__ = 'Finds a random user in a given channel'
+__module_author__ = 'Liam Stanley'
+
+import xchat, random
+
+help = 'Type /ruser then press enter to find a random user in the given channel.'
+
+print('%s has been loaded.' % __module_name__)
+
+def get_random(word, word_eol, userdata):
+    try:
+        list = xchat.get_list("users") 
+        if not list:
+            xchat.prnt(help)
+        selection = random.choice((list))
+        print '\x02User found! Username: \x0303%s\x0304%s\x03 (Connected via "\x0304%s\x03")\x02' % (selection.prefix, selection.nick, selection.host)
+    except:
+        xchat.prnt(help)
+
+def onUnload(userdata):
+    xchat.prnt('%s has been unloaded.' % __module_name__)
+
+xchat.hook_command('ruser', get_random, help=help)
+xchat.hook_unload(onUnload)

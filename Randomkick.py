ea176daa0a47a11e4c9c11dc9189fa4e7020dd45
Liam Stanley <me@liamstanley.net>
2013-12-29 02:11:31 -0500

    fix that Randomkick.py

--- Randomkick.py
+++ Randomkick.py
@@ -13,28 +13,28 @@ help = '%sType /rkick [nick] - to kick [user] with a random reason. ' % c + \
 print('%s%s has been loaded.' % (c, __module_name__))
 
 def rkick(word, word_eol, userdata):
-    try:
-        reason = select((
-                         'Goodbye!','See you later.','Cya.','Bye.','Later!',
-                         'Kindergarden is elsewhere!','Ugh. BYE!','G\'day',
-                         'Seeya later!','Be gone!','This is awkward. Bye.',
-                         'I didn\'t do it!'
-                        ))
-        if len(word) == 2:
-            # Assume they supplied a username
-            return xchat.command('kick %s %s' % (word[1], reason))
-        elif len(word) == 1:
-            # Assume they want a random user
-            list = xchat.get_list("users") 
-            if not list:
-                return xchat.prnt(help)
-            user = random.choice((list))
-            return xchat.command('kick %s %s' % (user.nick, reason))
-        else:
-            # Assume they entered some weird stuff
+    #try:
+    reason = select((
+                     'Goodbye!','See you later.','Cya.','Bye.','Later!',
+                     'Kindergarden is elsewhere!','Ugh. BYE!','G\'day',
+                     'Seeya later!','Be gone!','This is awkward. Bye.',
+                     'I didn\'t do it!'
+                    ))
+    if len(word) == 2:
+        # Assume they supplied a username
+        return xchat.command('kick %s %s' % (word[1], reason))
+    elif len(word) == 1:
+        # Assume they want a random user
+        list = xchat.get_list("users") 
+        if not list:
             return xchat.prnt(help)
-    except:
-        xchat.prnt(help)
+        user = select((list))
+        return xchat.command('kick %s %s' % (user.nick, reason))
+    else:
+        # Assume they entered some weird stuff
+        return xchat.prnt(help)
+    #except:
+    #    xchat.prnt(help)
 
 def onUnload(userdata):
     xchat.prnt('%s%s has been unloaded.' % (c, __module_name__))

95af83ac8183a65044655b65350bd68abe1d2c6a
Liam Stanley <me@liamstanley.net>
2013-12-28 19:15:54 -0500

    Move to v0.3 and rework plugin, much cleaner

--- Sexymeter.py
+++ Sexymeter.py
@@ -1,41 +1,40 @@
-__module_name__ = "Sexymeter"
-__module_version__ = "0.1"
-__module_description__ = "Detects your sexiness on a scale from 0 to 100."
-__module_author__ = "Jake0720"
+__module_name__ = 'Sexymeter Plugin'
+__module_version__ = '0.3'
+__module_description__ = 'Detects your sexiness on a scale from 0 to 100.'
+__module_author__ = 'Jake0720 with help from Liam Stanley'
 
 import xchat
-import random
+from random import randint as rand
 
 xchat.prnt(__module_name__+' has been loaded.')
+c = '\x02\x0303'
+help = '%sType /sm to see your sexiness on a scale from 0 to 100.' % c
+print('%s%s has been loaded.' % (c, __module_name__))
 
-smhelp = 'Type /sm to see your sexiness on a scale from 0 to 100.'
 
-def sm(word, word_eol, userdata):
-    sexyness = random.randint(0,100)
-    me = word[1]
-    if sexyness == 100:
-        xchat.command('say '+('\x02SEXY METER: '+'\x0306\x02'+str(sexyness)+' \x0309\x02Damn, you\'re so gorgeous you\'re fuckable, '+me+'!'))
-    elif sexyness >= 95 and sexyness < 99:
-        xchat.command('say '+('\x02SEXY METER: '+'\x0306\x02'+str(sexyness)+' \x0309\x02Damn, you\'re super sexy, '+me+'.'))
-    elif sexyness >= 80 and sexyness < 95:
-        xchat.command('say '+('\x02SEXY METER: '+'\x0306\x02'+str(sexyness)+'\x034\x02'+' Pretty sexy, '+me+'!'))
-    elif sexyness >= 65 and sexyness < 80:
-        xchat.command('say '+('\x02SEXY METER: '+'\x0306\x02'+str(sexyness)+'\x034\x02'+' Not bad, '+me+'!'))
-    elif sexyness >= 50 and sexyness < 65:
-        xchat.command('say '+('\x02SEXY METER: '+'\x0306\x02'+str(sexyness)+'\x034\x02'+' Could be better... \x0302@'+me))
-    elif sexyness >= 35 and sexyness < 50:
-        xchat.command('say '+('\x02SEXY METER: '+'\x0306\x02'+str(sexyness)+'\x034\x02'+' Eh... \x0302@'+me))
-    elif sexyness >= 20 and sexyness < 35:
-        xchat.command('say '+('\x02SEXY METER: '+'\x0306\x02'+str(sexyness)+'\x034\x02'+' Not so sexy... \x0302@'+me))
-    elif sexyness >= 5 and sexyness < 20:
-        xchat.command('say '+('\x02SEXY METER: '+'\x0306\x02'+str(sexyness)+'\x034\x02'+' Mirrors have been shattered, '+me+'!'))
-    elif sexyness >= 0 and sexyness < 5:
-        xchat.command('say '+('\x02SEXY METER: '+'\x0306\x02'+str(sexyness)+'\x034\x02'+' Holy shit, you\'re ugly, '+me+'!'))
+def sexymeter(word, word_eol, userdata):
+    sexyness = rand(0,100)
+    if len(word) == 2:
+        # Assume a username as args
+        user = word[1]
     else:
-        return
+        # Assume only the command, use their name!
+        user = xchat.get_info('nick')
+    
+    # Start comparing!
+    if sexyness == 100: r = 'Damn, you\'re so gorgeous you\'re fuckable'
+    elif sexyness >= 95: r = 'Damn, you\'re super sexy'
+    elif sexyness >= 80: r = 'Pretty sexy'
+    elif sexyness >= 65: r = 'Not bad'
+    elif sexyness >= 50: r = 'Could be better..'
+    elif sexyness >= 35: r = 'Eh..'
+    elif sexyness >= 20: r = 'Not soo sexy..'
+    elif sexyness >= 5: r = 'Mirrors have been shattered'
+    else: r = 'Holy shit, you\'re ugly'
+    return xchat.command('say \x02SEXYMETER:\x02 \x0306%s \x0304\x02%s %s' % (str(sexyness) + '%', r, user))
 
-def onUnload(unload):
-    xchat.prnt('\x034'+__module_name__+' has been unloaded.')
+def onUnload(userdata):
+    xchat.prnt('%s%s has been unloaded.' % (c, __module_name__))
 
-xchat.hook_command('sm', sm, help=smhelp)
+xchat.hook_command('sm', sexymeter, help=help)
 xchat.hook_unload(onUnload)
